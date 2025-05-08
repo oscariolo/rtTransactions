4c453a0236079e0ab49313409ece366ffeecbd46
oscar <oscar.orellana>
2025-05-08 09:42:19 -0500

    OO-ADD-Created spring project for distributed systems

---
+++ src/main/java/com/sistemasDistribuidos/rtTransactions/RtTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.sistemasDistribuidos.rtTransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemasDistribuidos/rtTransactions/RtTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemasDistribuidos.rtTransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
